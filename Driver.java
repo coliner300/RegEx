4421c785332036395d0537d2a7ecc5566c9c5976
coliner300 <coliner3000@gmail.com>
2021-02-08 01:45:06 +0000

    Finished main class, first constructor works

---
+++ Driver.java
@@ -0,0 +1,108 @@
+import java.util.Scanner;
+
+public class Driver
+{
+    private String bestPlayers;
+    private String bestTeams;
+    private String websites;
+    private String support;
+    private final Scanner scan = new Scanner(System.in);
+
+    public Driver(String bestPlayers, String bestTeams, String websites, String support)
+    {
+        this.bestPlayers = checkBestPlayers(bestPlayers);
+        this.bestTeams = checkBestTeams(bestTeams);
+        this.websites = checkWebsite(websites);
+        this.support = checkSupport(support);
+    }
+
+    public String getBestPlayers() 
+    { 
+      return bestPlayers; 
+    }
+
+    public String getBestTeams() {
+        return bestTeams;
+    }
+
+    public String getWebsite() 
+    { 
+      return websites; 
+    }
+    public String getSupport() 
+    { 
+      return support; 
+    }
+
+
+    public void setBestPlayers(String bestPlayers) 
+    {
+        this.bestPlayers = checkBestPlayers(bestPlayers);
+    }
+
+    public void setBestTeams(String bestTeams) 
+    {
+        this.bestTeams = checkBestTeams(bestTeams);
+    }
+
+    public void setWebsites(String websites) 
+    {
+        this.websites = checkWebsite(websites);
+    }
+
+    public void setSupport(String support)
+    {
+        this.support = checkSupport(support);
+    }
+
+    private String checkBestPlayers(String name)
+    {
+        while(!name.matches("[A-Z][A-Za-z]+"))
+        {
+            System.out.println("Best Players: Invalid , please type correctly");
+            name = scan.next();
+        }
+        return name;
+    }
+
+    private String checkBestTeams(String name)
+    {
+        while(!name.matches("[A-Z]([A-Za-z]|-)+"))
+        {
+            System.out.println("Best Teams: Invalid , please type correctly");
+            name = scan.next();
+        }
+        return name;
+    }
+
+   
+    private String checkWebsite(String email)
+    {
+        while(!email.matches("[A-Za-z0-9]+@[a-z]+\\.(com|net|edu)"))
+        {
+            System.out.println("Websites: Invalid , please type correctly");
+            email = scan.next();
+        }
+        return email;
+    }
+
+    private String checkSupport(String num)
+    {
+        while(!num.matches("\\([0-9]{3}\\)[0-9]{3}-[0-9]{4}"))
+        {
+            System.out.println("Support: Invalid , please type correctly");
+            num = scan.next();
+        }
+        return num;
+    }
+
+    public String toString()
+    {
+        String output;
+        output  = "Best Player: " + bestPlayers;
+        output += "\nBest Teams: " + bestTeams;
+        output += "\nWebsite: " + websites;
+        output += "\nSupport: " + support;
+        return output;
+    }
+}
--- Main.java
+++ Main.java
@@ -1,104 +1,115 @@
-import java.util.Scanner;
-
 public class Main
 {
-  private String bestPlayers; //letters
-  private String bestTeams; //letters
-  private String websites; //special characters
-  private String support; //numbers
-  private final Scanner scan = new Scanner(System.in);
-
-
-public Main(String bestPlayers, String bestTeams, String records, String websites)
-{
-  this.bestPlayers = checkBestPlayers(bestPlayers);
-  this.bestTeams = checkBestTeams(bestTeams);
-  this.websites = checkWebsites(websites);
-  this.support = checkSupport(support);
-}//end constructor
-
-public String getBestPlayers()
-{
-  return bestPlayers;
-}
-public String getBestTeams()
-{
-  return bestTeams;
-}
-public String getWebsites()
-{
-  return websites;
-}
-public String getSupport()
-{
-  return support;
-}
-
-public void setBestPlayers()
-{
-  this.bestPlayers = checkBestPlayers(bestPlayers);
-}
-public void setBestTeams()
-{
-  this.bestTeams = checkBestTeams(bestTeams);
-}
-public void setWebsites()
-{
-  this.websites = checkWebsites(websites);
-}
-public void setSupport()
-{
-  this.support = checkSupport(support);
-}
-
-
-private String checkBestPlayers(String name)
-{
-  while(!name.matches("[A-Z][A-Za-z]+"))
+  public static void main(String[] args)
   {
-    System.out.println("Best Players: Invalid, please type correctly");
-    bestPlayers = scan.next();
-  }
-  return bestPlayers;
-}
-
-private String checkBestTeams(String name)
-{
-  while(!name.matches("[A-Z][A-Za-z]+"))
-  {
-    System.out.println("Best Teams: Invalid, please type correctly");
-    bestTeams = scan.next();
-  }
-  return bestTeams;
-}
-
-private String checkWebsites(String email)
-{
-  while(!email.matches("[A-Za-z0-9]+@[a-z]+\\.(com\|net|edu)"))
-  {
-    System.out.println("Websites: Invalid, please type correctly");
-    websites = scan.next();
+    Driver miscellaneous = new Driver("Barry", "Yankees", "mlb@baseball.net", "(808)452-8456");
+    System.out.println(miscellaneous.toString());
   }
 }
 
-private String checkSupport(String num)
-{
-  while(!num.matches("\\([0-9]{3}\\")[0-9]{3}-[0-9]{4}))
-  {
-    System.out.println("Support Number: Invalid, please type correctly");
-    support = scan.next();
-  }
-  return support;
-}
 
-public String toString()
-{
-  String output;
-  output += "Best Players: " + bestPlayers;
-  output += "\nBest Teams: " + bestTeams;
-  output += "\nSupport: " + support;
-  output += "\nWebsites: " + websites;
-  return output;
-}
+// import java.util.Scanner;
 
-}//end class main
+// public class Main
+// {
+//   private String bestPlayers; //letters
+//   private String bestTeams; //letters
+//   private String websites; //special characters
+//   private String support; //numbers
+//   private final Scanner scan = new Scanner(System.in);
+
+
+// public Main(String bestPlayers, String bestTeams, String records, String websites)
+// {
+//   this.bestPlayers = checkBestPlayers(bestPlayers);
+//   this.bestTeams = checkBestTeams(bestTeams);
+//   this.websites = checkWebsites(websites);
+//   this.support = checkSupport(support);
+// }//end constructor
+
+// public String getBestPlayers()
+// {
+//   return bestPlayers;
+// }
+// public String getBestTeams()
+// {
+//   return bestTeams;
+// }
+// public String getWebsites()
+// {
+//   return websites;
+// }
+// public String getSupport()
+// {
+//   return support;
+// }
+
+// public void setBestPlayers()
+// {
+//   this.bestPlayers = checkBestPlayers(bestPlayers);
+// }
+// public void setBestTeams()
+// {
+//   this.bestTeams = checkBestTeams(bestTeams);
+// }
+// public void setWebsites()
+// {
+//   this.websites = checkWebsites(websites);
+// }
+// public void setSupport()
+// {
+//   this.support = checkSupport(support);
+// }
+
+
+// private String checkBestPlayers(String name)
+// {
+//   while(!name.matches("[A-Z][A-Za-z]+"))
+//   {
+//     System.out.println("Best Players: Invalid, please type correctly");
+//     bestPlayers = scan.next();
+//   }
+//   return bestPlayers;
+// }
+
+// private String checkBestTeams(String name)
+// {
+//   while(!name.matches("[A-Z][A-Za-z]+"))
+//   {
+//     System.out.println("Best Teams: Invalid, please type correctly");
+//     bestTeams = scan.next();
+//   }
+//   return bestTeams;
+// }
+
+// private String checkWebsites(String email)
+// {
+//   while(!email.matches("[A-Za-z0-9]+@[a-z]+\\.(com|net|edu)"))
+//   {
+//     System.out.println("Websites: Invalid, please type correctly");
+//     websites = scan.next();
+//   }
+//   return websites;
+// }
+
+// private String checkSupport(String num)
+// {
+//   while(!num.matches("[0-9]{3}-[0-9]{3}-[0-9]{4}))"))
+//   {
+//     System.out.println("Support Number: Invalid, please type correctly");
+//     support = scan.next();
+//   }
+//   return support;
+// }
+
+// public String toString()
+// {
+//   String output;
+//   output = "Best Players: " + bestPlayers;
+//   output += "\nBest Teams: " + bestTeams;
+//   output += "\nSupport: " + support;
+//   output += "\nWebsites: " + websites;
+//   return output;
+// }
+
+// }//end class main
